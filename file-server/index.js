782ba0649b0f3acbe19d469a55379f4f7c7e92be
Calibur <shahoriar21@gmail.com>
2026-04-03 23:56:54 +0600

    max file size increased to 100MB

--- file-server/index.js
+++ file-server/index.js
@@ -67,7 +67,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage,
   limits: {
-    fileSize: 10 * 1024 * 1024, // 10 MB
+    fileSize: 100 * 1024 * 1024, // 100 MB
   },
   fileFilter: (req, file, cb) => {
     // Allowed MIME types
